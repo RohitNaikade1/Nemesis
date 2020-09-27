72ec8a6d4e5974a7c505f3e7229428773910401d
Bhole Sarkaar <naikaderohit833@gmail.com>
2020-09-27 15:37:27 +0530

    final submit 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route,Switch,BrowserRouter as Router} from 'react-router-dom';
